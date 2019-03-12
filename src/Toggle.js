33d6570bb7c5370186b8aba0e456a9cc8a42cfea
Vassil Genov <vg@Vassils-MacBook-Air-2.local>
2019-03-12 20:50:08 +0200

    Revert "Updated eslint and prettier config"
    This reverts commit 874702dbadf9e8c598f26940914e72e7970c1a80.

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
 
 export default class Toggle extends Component {
-    static propTypes = {
-        render: PropTypes.func.isRequired,
-    };
+  state = {
+    on: false
+  };
 
-    state = {
-        on: false,
-    };
+  toggle = () => {
+    this.setState(state => ({
+      on: !state.on
+    }));
+  };
 
-    toggle = () => {
-        this.setState(state => ({
-            on: !state.on,
-        }));
-    };
+  render() {
+    const { render } = this.props;
+    const { on } = this.state;
+    const { toggle } = this;
 
-    render() {
-        const { render } = this.props;
-        const { on } = this.state;
-        const { toggle } = this;
-
-        return <>{render({ on, toggle })}</>;
-    }
+    return <>{render({ on, toggle })}</>;
+  }
 }
--- src/ToggleRPC.js
+++ src/ToggleRPC.js
@@ -1,26 +1,21 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Component } from "react";
 
 export default class ToggleRPC extends Component {
-    static propTypes = {
-        children: PropTypes.func.isRequired,
-    };
+  state = {
+    on: false
+  };
 
-    state = {
-        on: false,
-    };
+  toggle = () => {
+    this.setState(state => ({
+      on: !state.on
+    }));
+  };
 
-    toggle = () => {
-        this.setState(state => ({
-            on: !state.on,
-        }));
-    };
+  render() {
+    const { children } = this.props;
+    const { on } = this.state;
+    const { toggle } = this;
 
-    render() {
-        const { children } = this.props;
-        const { on } = this.state;
-        const { toggle } = this;
-
-        return children({ on, toggle });
-    }
+    return children({ on, toggle });
+  }
 }
