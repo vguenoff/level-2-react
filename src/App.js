77e615f2c92378158d145984cdb1e86663e9aab3
Vassil Genov <vg@Vassils-MacBook-Air-2.local>
2019-03-12 20:52:18 +0200

    Revert "Render props and render props children. Prettier added"
    This reverts commit 5e8d665151e9e51a6aa25b01594bcc406a220d8b.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-import Toggle from "./Toggle";
-import ToggleRPC from "./ToggleRPC";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -11,22 +8,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Toggle
-            render={({ on, toggle }) => (
-              <>
-                {on && <h1>Hello Scott</h1>}
-                <button onClick={toggle}>Show/hide</button>
-              </>
-            )}
-          />
-          <ToggleRPC>
-            {({ on, toggle }) => (
-              <>
-                {on && <h1>Render Props Children</h1>}
-                <button onClick={toggle}>Show/hide</button>
-              </>
-            )}
-          </ToggleRPC>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
