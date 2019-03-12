874702dbadf9e8c598f26940914e72e7970c1a80
Vassil Genov <vg@Vassils-MacBook-Air-2.local>
2019-03-12 20:37:21 +0200

    Updated eslint and prettier config

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Toggle from "./Toggle";
-import ToggleRPC from "./ToggleRPC";
+import Toggle from './Toggle';
+import ToggleRPC from './ToggleRPC';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Toggle
-            render={({ on, toggle }) => (
-              <>
-                {on && <h1>Hello Scott</h1>}
-                <button onClick={toggle}>Show/hide</button>
-              </>
-            )}
-          />
-          <ToggleRPC>
-            {({ on, toggle }) => (
-              <>
-                {on && <h1>Render Props Children</h1>}
-                <button onClick={toggle}>Show/hide</button>
-              </>
-            )}
-          </ToggleRPC>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <Toggle
+                        render={({ on, toggle }) => (
+                            <>
+                                {on && <h1>Hello Scott</h1>}
+                                <button onClick={toggle}>Show/hide</button>
+                            </>
+                        )}
+                    />
+                    <ToggleRPC>
+                        {({ on, toggle }) => (
+                            <>
+                                {on && <h1>Render Props Children</h1>}
+                                <button onClick={toggle}>Show/hide</button>
+                            </>
+                        )}
+                    </ToggleRPC>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
